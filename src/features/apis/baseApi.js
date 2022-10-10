a9a24877c2ea098965a6140bd66884db1bcbc7bd
sepehrzmn <szamani3045@gmail.com>
2022-10-10 09:42:54 -0400

    setup get config an setup redux

---
+++ src/features/apis/baseApi.js
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApiSlice = createApi({
+    reducerPath: "baseApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "https://www.namava.ir/api" }),
+    endpoints: (builder) => ({
+        getConfig: builder.query({
+            query: () => "/v1.0/applications/configs",
+        }),
+        getMenu: builder.query({
+            query: () => "/v3.0/menus",
+        }),
+    }),
+});
+
+export const { useGetConfigQuery } = baseApiSlice;
